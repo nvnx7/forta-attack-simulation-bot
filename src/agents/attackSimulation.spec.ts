ad04c1c966a4fff3738a794c5a9aff3b55750828
theNvN <thenvnsahu@gmail.com>
2022-07-06 20:11:40 +0530

    refactored & fixed attack simulation

--- src/agents/attackSimulation.spec.ts
+++ src/agents/attackSimulation.spec.ts
@@ -1,31 +1,34 @@
-import { createTransactionEvent, HandleTransaction, TransactionEvent } from 'forta-agent';
+import { createTransactionEvent, ethers, Finding, FindingSeverity, FindingType } from 'forta-agent';
 import { getEthersForkProvider } from '../utils/blockchain';
 import attackSimAgent from './attackSimulation';
 
-const mockNonAttacker = '0x1234567890123456789012345678901234567890';
-const mockAttacker = '0x63341Ba917De90498F3903B199Df5699b4a55AC0'; // exploiter
-const mockAttackerContract = '0x7336F819775B1D31Ea472681D70cE7A903482191'; // exploiter
+const mockAttacker = '0x63341ba917de90498f3903b199df5699b4a55ac0';
+const mockAttackerContract = '0x7336f819775b1d31ea472681d70ce7a903482191';
+const mockBytecode = '0x8063af8271f71461abcd57';
+const mockTokenDataToCheck = [
+  { address: '', alertDeltaThreshold: '100' }, // Assumes native ETH if address is empty
+  { address: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', alertDeltaThreshold: '100' }, // WETH
+];
 const blockNumber = 14684300;
 const chainId = 1;
 
+const zero = ethers.utils.parseEther('0');
+const twoHundred = ethers.utils.parseEther('200');
+
 describe.only('attack simulation', () => {
-  let handleTx: HandleTransaction;
-  let mockTxEvent: TransactionEvent;
-
-  beforeAll(async () => {
-    handleTx = attackSimAgent.provideHandleTx(chainId, getEthersForkProvider);
-    mockTxEvent = createTransactionEvent({
-      transaction: { from: mockAttacker },
-      contractAddress: mockAttackerContract,
-      block: { number: blockNumber },
-    } as any);
-  });
-
   describe('handleTransaction', () => {
     jest.setTimeout(40000);
-    it('return empty finding if provided contract address is not actually a contract (0 code)', async () => {
+    it('return empty finding if tx is not actually contract creation tx', async () => {
+      const mockGetTxReceipt = jest.fn().mockResolvedValue({ contractAddress: null });
+      const mockGetMultiCallProvider = jest.fn();
+      const handleTx = attackSimAgent.provideHandleTx(
+        chainId,
+        getEthersForkProvider,
+        mockGetTxReceipt,
+        mockGetMultiCallProvider,
+        mockTokenDataToCheck,
+      );
       const mockTxEvent = createTransactionEvent({
-        contractAddress: mockNonAttacker,
         transaction: { from: mockAttacker },
         block: { number: blockNumber },
       } as any);
@@ -35,17 +38,81 @@ describe.only('attack simulation', () => {
 
     it('returns empty finding if any balance change below the threshold occur', async () => {
       const mockTxEvent = createTransactionEvent({
-        contractAddress: mockAttackerContract,
-        transaction: { from: mockNonAttacker },
+        transaction: { from: mockAttacker, to: null },
         block: { number: blockNumber },
       } as any);
+
+      const mockGetTxReceipt = jest
+        .fn()
+        .mockResolvedValue({ contractAddress: mockAttackerContract });
+      const mockGetMultiCallProvider = jest.fn().mockReturnValue({
+        getEthBalance: jest.fn(),
+        all: jest.fn().mockResolvedValueOnce([zero, zero]).mockResolvedValue([zero, zero]),
+      });
+      const mockGetEthersForkProvider = jest.fn().mockReturnValue({
+        getCode: jest.fn().mockResolvedValue(mockBytecode),
+        getSigner: jest.fn().mockReturnValue({
+          sendTransaction: jest.fn().mockResolvedValue({}),
+        }),
+      });
+      const handleTx = attackSimAgent.provideHandleTx(
+        chainId,
+        mockGetEthersForkProvider,
+        mockGetTxReceipt,
+        mockGetMultiCallProvider,
+        mockTokenDataToCheck,
+      );
       let findings = await handleTx(mockTxEvent);
       expect(findings).toStrictEqual([]);
     });
 
-    // it('works', async () => {
-    //   const findings = await handleTx(mockTxEvent);
-    //   expect(true).toEqual(true);
-    // });
+    it('raises alert if detects large amount of transfers', async () => {
+      const mockTxEvent = createTransactionEvent({
+        transaction: { from: mockAttacker, to: null },
+        block: { number: blockNumber },
+      } as any);
+
+      const mockGetTxReceipt = jest
+        .fn()
+        .mockResolvedValue({ contractAddress: mockAttackerContract });
+      const mockGetMultiCallProvider = jest.fn().mockReturnValue({
+        getEthBalance: jest.fn(),
+        all: jest
+          .fn()
+          // Set mock values such that only the attacker's balance of the token(weth)
+          // hits the threshold
+          .mockResolvedValueOnce([zero, zero, zero, zero])
+          .mockResolvedValue([zero, twoHundred, zero, zero]),
+      });
+      const mockGetEthersForkProvider = jest.fn().mockReturnValue({
+        getCode: jest.fn().mockResolvedValue(mockBytecode),
+        getSigner: jest.fn().mockReturnValue({
+          sendTransaction: jest.fn().mockResolvedValue({}),
+        }),
+      });
+      const handleTx = attackSimAgent.provideHandleTx(
+        chainId,
+        mockGetEthersForkProvider,
+        mockGetTxReceipt,
+        mockGetMultiCallProvider,
+        mockTokenDataToCheck,
+      );
+      let findings = await handleTx(mockTxEvent);
+      expect(findings).toStrictEqual([
+        Finding.fromObject({
+          name: 'Potential High Value Transfer Exploit',
+          description: `Potential high value drain detected from suspicious address - ${mockAttacker}`,
+          alertId: attackSimAgent.ALERT_ID,
+          severity: FindingSeverity.Critical,
+          type: FindingType.Exploit,
+          metadata: {
+            attacker: mockAttacker,
+            attackerContract: mockAttackerContract,
+            token: mockTokenDataToCheck[1].address,
+            transferValue: twoHundred.toString(),
+          },
+        }),
+      ]);
+    });
   });
 });
--- src/agents/attackSimulation.ts
+++ src/agents/attackSimulation.ts
@@ -3,36 +3,33 @@ import {
   Finding,
   FindingSeverity,
   FindingType,
-  getTransactionReceipt,
   HandleTransaction,
+  Receipt,
   TransactionEvent,
 } from 'forta-agent';
-import { Contract, Provider } from 'ethers-multicall';
+import { Contract } from 'ethers-multicall';
 
-import {
-  ETH_DELTA_THRESHOLD,
-  TOKEN_ADDRESSES_TO_CHECK,
-  TOKEN_BALANCE_ABI,
-} from '../utils/constants';
-import { analyzeBytecode, GetEthersForkProvider } from '../utils/blockchain';
+import { TOKEN_BALANCE_ABI } from '../utils/constants';
+import { analyzeBytecode, GetEthersForkProvider, GetMultiCallProvider } from '../utils/blockchain';
 
 const ALERT_ID = 'MALICIOUS_TRANSACTION_SIMULATION';
-
-const ethDeltaThreshold = ethers.utils.parseEther(`${ETH_DELTA_THRESHOLD}`);
-const tokenDeltaThresholds = TOKEN_ADDRESSES_TO_CHECK.map((v) =>
-  ethers.utils.parseEther(`${v.alertDeltaThreshold}`),
-);
-const deltaThresholds = [ethDeltaThreshold, ...tokenDeltaThresholds];
 
 const provideHandleTx = (
   chainId: number,
   getEthersForkProvider: GetEthersForkProvider,
+  getTxReceipt: (txHash: string) => Promise<Receipt>,
+  getMultiCallProvider: GetMultiCallProvider,
+  tokenDataToCheck: { address: string; alertDeltaThreshold: string }[],
 ): HandleTransaction => {
   return async function handleTx(txEvent: TransactionEvent) {
     const findings: Finding[] = [];
+    // check for contract creation tx
+    if (txEvent.to) {
+      return findings;
+    }
 
     const attacker = txEvent.from.toLowerCase();
-    const attackerContract = await getTransactionReceipt(txEvent.hash)
+    const attackerContract = await getTxReceipt(txEvent.hash)
       .then((tx) => tx.contractAddress)
       .catch(() => {
         console.error(`Failed to get contract address for tx - ${txEvent.hash}`);
@@ -46,57 +43,103 @@ const provideHandleTx = (
     const blockNumber = txEvent.block.number;
     const provider = getEthersForkProvider(blockNumber, [attacker]);
 
-    const signer = provider.getSigner(attacker);
-
-    // Check if address is contract
+    // Check if address is contract i.e. bytecode not empty
     const bytecode = await provider.getCode(attackerContract);
-
     if (bytecode === '0x') {
       return findings;
     }
 
     // Extract functions selectors from bytecode
     const functionsSelectors = analyzeBytecode(bytecode);
-
     if (functionsSelectors.length === 0) {
       return findings;
     }
 
-    const multiCallProvider = new Provider(provider, chainId);
+    const signer = provider.getSigner(attacker);
+    const multiCallProvider = getMultiCallProvider(provider, chainId);
 
-    const ethBalanceCall = multiCallProvider.getEthBalance(attacker);
-    const tokenBalanceCalls = TOKEN_ADDRESSES_TO_CHECK.map((v) => {
-      const contract = new Contract(v.address, [TOKEN_BALANCE_ABI]);
-      return contract.balanceOf(attacker);
+    // Collect all balance call objects in a single batch call
+    // const attackerBalCalls: any = [multiCallProvider.getEthBalance(attacker)];
+    // const attackerContractBalCalls: any = [multiCallProvider.getEthBalance(attackerContract)];
+    const attackerBalCalls: any = [];
+    const attackerContractBalCalls: any = [];
+    tokenDataToCheck.forEach((tok) => {
+      if (!tok.address) {
+        // Assume native eth
+        attackerBalCalls.push(multiCallProvider.getEthBalance(attacker));
+        attackerContractBalCalls.push(multiCallProvider.getEthBalance(attackerContract));
+      } else {
+        // A token
+        const contract = new Contract(tok.address, [TOKEN_BALANCE_ABI]);
+        attackerBalCalls.push(contract.balanceOf(attacker));
+        attackerContractBalCalls.push(contract.balanceOf(attackerContract));
+      }
     });
-    const balanceCalls = [ethBalanceCall, ...tokenBalanceCalls];
 
-    const startBalances = await multiCallProvider.all(balanceCalls);
-    for (const selector of functionsSelectors.slice(1)) {
-      await signer
-        .sendTransaction({
+    // First half of the calls are of `attacker` address
+    // Second half of the calls are of `attackerContract` address
+    const allBalanceCalls = [...attackerBalCalls, ...attackerContractBalCalls];
+
+    // Fetch all initial balances in a single call
+    const startBalances = await multiCallProvider.all(allBalanceCalls);
+
+    // Balance change thresholds to fire alerts
+    const deltaThresholds = tokenDataToCheck.map((tok) =>
+      ethers.utils.parseEther(tok.alertDeltaThreshold),
+    );
+    for (const selector of functionsSelectors) {
+      try {
+        await signer.sendTransaction({
           to: attackerContract,
           data: `0x${selector}`,
-        })
-        // ignore reverts
-        .catch(() => {});
+        });
+      } catch (error) {
+        // ignore errors/reverts
+        continue;
+      }
 
-      const endBalances = await multiCallProvider.all(balanceCalls);
+      // Fetch all balances after the call
+      const endBalances = await multiCallProvider.all(allBalanceCalls);
 
-      // Asses balance changes and report findings
+      // Calculate all balance changes
       const deltas = startBalances.map((startBal, i) => endBalances[i].sub(startBal));
-      deltas.forEach((delta, i) => {
+
+      // Check balance changes above threshold for `attacker` address
+      const attackerDeltas = deltas.slice(0, deltas.length / 2);
+      attackerDeltas.forEach((delta, i) => {
         if (delta.gte(deltaThresholds[i])) {
           const finding = Finding.fromObject({
-            name: 'Suspicious High Value Transfer',
-            description: `High value transfer detected from suspicious address - ${attacker}`,
+            name: 'Potential High Value Transfer Exploit',
+            description: `Potential high value drain detected from suspicious address - ${attacker}`,
             alertId: ALERT_ID,
             severity: FindingSeverity.Critical,
             type: FindingType.Exploit,
             metadata: {
               attacker,
               attackerContract,
-              token: i === 0 ? 'native' : TOKEN_ADDRESSES_TO_CHECK[i - 1].address,
+              token: tokenDataToCheck[i].address || 'native',
+              transferValue: delta.toString(),
+            },
+          });
+
+          findings.push(finding);
+        }
+      });
+
+      // Check balance changes above threshold for `attackerContract` address
+      const attackerContractDeltas = deltas.slice(deltas.length / 2);
+      attackerContractDeltas.forEach((delta, i) => {
+        if (delta.gte(deltaThresholds[i])) {
+          const finding = Finding.fromObject({
+            name: 'Potential High Value Transfer Exploit',
+            description: `Potential high value drain detected from suspicious address - ${attackerContract}`,
+            alertId: ALERT_ID,
+            severity: FindingSeverity.Critical,
+            type: FindingType.Exploit,
+            metadata: {
+              attacker,
+              attackerContract,
+              token: tokenDataToCheck[i].address || 'native',
               transferValue: delta.toString(),
             },
           });
